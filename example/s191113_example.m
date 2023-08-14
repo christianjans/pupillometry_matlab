ef9109e959ed4d1933dd0a891d15977ad6e6e30a
Christian Jans <cjjans@ualberta.ca>
2023-08-14 09:27:05 -0600

    Update example code

--- example/s191113_example.m
+++ example/s191113_example.m
@@ -1,5 +1,5 @@
 % Add pupil-analysis to MATLAB path
-addpath(genpath('D:\Code\Matlab\pupil-analysis'))
+addpath(genpath('/Users/christian/Documents/summer2023/pupillometry_matlab'))
 
 % Define parameters
 fitMethod = 2;
@@ -9,8 +9,9 @@ thresVal = [];
 frameInterval = 2;
 videoPath = fullfile(utils.get_rootdir, 'example/sample_video.mp4');
 fileSavePath = fullfile(utils.get_rootdir, 'example');
-startFrame = 5;
+startFrame = 1;
 enhanceContrast = true;
+brightenBeforeContrast = false;
 doCrop = true;
 skipBadFrames = true;
 fillBadData = 'linear';
@@ -22,6 +23,6 @@ pup02 = pupilMeasurement('fitMethod', fitMethod, 'doPlot', doPlot, ...
     'thresVal', thresVal, 'frameInterval', frameInterval, ...
     'videoPath', videoPath, 'fileSavePath', fileSavePath, ...
     'startFrame', startFrame, 'enhanceContrast', enhanceContrast, ...
-    'doCrop', doCrop, 'skipBadFrames', skipBadFrames, ...
-    'fillBadData', fillBadData, 'saveLabeledFrames', saveLabeledFrames, ...
-    'pixelSize', pixelSize)
+    'brightenBeforeContrast', brightenBeforeContrast, 'doCrop', doCrop, ...
+    'skipBadFrames', skipBadFrames, 'fillBadData', fillBadData, ...
+    'saveLabeledFrames', saveLabeledFrames, 'pixelSize', pixelSize)
